14dbb129161d25a411ad1816b6e42baeeb8daf28
Muhammad Irtiza <m7.irtiza@gmail.com>
2021-12-29 18:29:04 +0500

    Implemented Splay Tree.

---
+++ Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String [] args) {
+        Splay_Tree tree = new Splay_Tree();
+        tree.insert(33);
+        tree.insert(44);
+        tree.insert(67);
+        tree.insert(5);
+        tree.insert(89);
+        tree.insert(41);
+        tree.insert(98);
+        tree.insert(1);
+        tree.prettyPrint();
+        tree.searchTree(33);
+        tree.searchTree(44);
+        tree.prettyPrint();
+        tree.deleteNode(89);
+        tree.deleteNode(67);
+        tree.deleteNode(41);
+        tree.deleteNode(5);
+        tree.prettyPrint();
+        tree.deleteNode(98);
+        tree.deleteNode(1);
+        tree.deleteNode(44);
+        tree.deleteNode(33);
+        tree.prettyPrint();
+    }
+}
---
+++ Node.java
@@ -0,0 +1,13 @@
+class Node {
+    int data;
+    Node parent;
+    Node left;
+    Node right;
+
+    public Node(int data) {
+        this.data = data;
+        this.parent = null;
+        this.left = null;
+        this.right = null;
+    }
+}
